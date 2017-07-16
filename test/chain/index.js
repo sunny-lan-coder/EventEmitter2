3039acef50ffb799c09d1f9eb769e1b525610bb2
Sunny <sunny.lan.coder@gmail.com>
2017-07-15 22:50:48 -0400

    Bugfix with more than 3 event params

--- test/chain/index.js
+++ test/chain/index.js
@@ -2,8 +2,8 @@ const E = require('../../').EventEmitterChain2;
 
 let e = new E({wildcard: true}, (evt) => console.log("terminator called upon event ", evt));
 
-e.on('a', (next) => {
-    console.log("handler 1 was triggered (a)");
+e.on('a', (next, x, y, z, a) => {
+    console.log("handler 1 was triggered (a)", x, y, z, a);
     next();
 });
 
@@ -20,10 +20,10 @@ e.on('b', (next) => {
 e.on('c', () => console.log("handler 4 was triggered (c). not calling next"));
 
 console.log("emitting a");
-e.emit('a');
+e.emit('a', 1, 2, 3, 4);
 console.log("emitting b");
 e.emit('b');
-console.log("emitting *");
+console.log("emitting *", 5, 6, 7, 8);
 e.emit('*');
 console.log("emitting c");
 e.emit('c');
